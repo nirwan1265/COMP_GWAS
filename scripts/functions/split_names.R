c822335673f90eb9a0c542d85c739601b4802a9f
Nirwan Tandukar <88001815+nirwan1265@users.noreply.github.com>
2022-11-11 10:33:00 -0500

    Create split_names.R

---
+++ scripts/functions/split_names.R
@@ -0,0 +1,7 @@
+# Subsetting just the genename from the gff3 files
+
+split.names <- function(x,split){
+  split.genename <- unlist(strsplit(x, split = ';', fixed = TRUE))[1]
+  split.genename2 <- unlist(strsplit(split.genename, split = ":", fixed = TRUE))[2]
+  return(split.genename2)
+}
