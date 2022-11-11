2b865a991c6ec3decaa3f98fd58d2e4af4c245f7
Nirwan Tandukar <88001815+nirwan1265@users.noreply.github.com>
2022-11-11 13:37:19 -0500

    Update subsetting_chromosome.sh

--- scripts/subsetting_chromosome.sh
+++ scripts/subsetting_chromosome.sh
@@ -6,3 +6,5 @@ done
 
 # $2, $1, $3 and $11 represents markers, chromosome, snp position, pvalues in the gwas file
 # run the script in the folder where your gwas results are. 
+# the script converts all gwas files in the folder 
+
