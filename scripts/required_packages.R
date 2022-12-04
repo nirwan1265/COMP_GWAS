12d50727ef8dbb0caf93ea3fec2076e80dd5eb32
nirwan1265 <tandukar.nirwan@gmail.com>
2022-12-04 18:20:13 -0500

    update

---
+++ scripts/required_packages.R
@@ -0,0 +1,20 @@
+# Package names
+packages <- c("tidyverse","ggplot2", "Rsamtools","GenomicAlignments","rtracklayer","GenomicRanges","AnnotationHub","knitr","gtools","data.table","stringi","GBJ","metap","multtest","Hmisc","devtools","SNPRelate","gdsfmt","dplyr","vcfR","tidyr","AssocTests","SKAT","NCmisc","ACAT","PANTHER.db","UniProt.ws","ape","raster","sp","rgdal","rworldmap","janitor","countrycode","tibble","vroom")
+
+# Install packages not yet installed
+# installed_packages <- packages %in% rownames(installed.packages())
+# if (any(installed_packages == FALSE)) {
+#  install.packages(packages[!installed_packages])
+# }
+# if (any(installed_packages == FALSE)) {
+#   BiocManager::install(packages[!installed_packages])
+# }
+# devtools::install_github("yaowuliu/ACAT")
+# remotes::install_github("yaowuliu/ACAT")
+# install.packages("/Users/nirwantandukar/Documents/Sorghum root rnaseq data_low phosphorus/org.Sbicolor.eg.db", repos=NULL, type="source")
+
+
+# Packages loading
+invisible(lapply(packages, library, character.only = TRUE))
+
+
