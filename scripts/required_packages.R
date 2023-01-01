d4623cfacb28fca72b5f54bad5b633affe91b351
tandukar.nirwan@gmail.com <88001815+nirwan1265@users.noreply.github.com>
2023-01-01 17:33:39 -0600

    update

--- scripts/required_packages.R
+++ scripts/required_packages.R
@@ -1,5 +1,5 @@
 # Package names
-packages <- c("tidyverse","ggplot2", "Rsamtools","GenomicAlignments","rtracklayer","GenomicRanges","AnnotationHub","knitr","gtools","data.table","stringi","GBJ","metap","multtest","Hmisc","devtools","SNPRelate","gdsfmt","dplyr","vcfR","tidyr","AssocTests","SKAT","NCmisc","ACAT","PANTHER.db","UniProt.ws","ape","raster","sp","rgdal","rworldmap","janitor","countrycode","tibble","vroom")
+packages <- c("tidyverse","ggplot2", "Rsamtools","GenomicAlignments","rtracklayer","GenomicRanges","AnnotationHub","knitr","gtools","data.table","stringi","GBJ","metap","multtest","Hmisc","devtools","SNPRelate","gdsfmt","dplyr","vcfR","tidyr","AssocTests","SKAT","NCmisc","ACAT","PANTHER.db","UniProt.ws","ape","raster","sp","rgdal","rworldmap","janitor","countrycode","tibble","vroom","gtools")
 
 # Install packages not yet installed
 # installed_packages <- packages %in% rownames(installed.packages())
@@ -16,5 +16,3 @@ packages <- c("tidyverse","ggplot2", "Rsamtools","GenomicAlignments","rtracklaye
 
 # Packages loading
 suppressMessages(invisible(lapply(packages, library, character.only = TRUE)))
-
-
