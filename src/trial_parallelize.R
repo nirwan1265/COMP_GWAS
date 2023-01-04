d9d1089e312f893995c7c74f1499adb123ba1fa3
nirwan1265 <tandukar.nirwan@gmail.com>
2023-01-04 15:58:24 -0500

    update

--- src/trial_parallelize.R
+++ src/trial_parallelize.R
@@ -44,13 +44,12 @@ gbj_test <- function(gwas.zstat, gwas.marker, gwas.pvalue, geno, tab.pc){
 
 for(i in chr)
 zstat_df <- as.data.frame(trial$preprocess$Zstat[[1]]) # i
-zstat_df <- zstat_df[1:5,1:5]
+zstat_df <- zstat_df[1:100,1:100]
 pvalue_df <- as.data.frame(trial$preprocess$pvalue[[1]])
-pvalue_df <- pvalue_df[1:5,1:5]
+pvalue_df <- pvalue_df[1:100,1:100]
 marker_df <- as.data.frame(trial$preprocess$Marker[[1]])
-marker_df <- marker_df[1:5,1:5]
+marker_df <- marker_df[1:100,1:100]
 genotype_df <- as.data.frame(trial$genotype[[1]])
-ref_genotype <- as.data.frame(genontype_df[,colnames(genontype_df)] %in% marker_df)
 
 trial_add <- function(x){
   return(x+100)
@@ -67,21 +66,11 @@ trial_ref <- function(y,z){
 # Use foreach to apply the function to each column in parallel
 #results <- foreach(i = 1:ncol(as.data.frame(trial$preprocess$Zstat[1])), .combine = c) %dopar% {
 results = list()
-results <- foreach(i = 1:3, .combine = c) %dopar% {
+tic()
+results <- foreach(i = 1:100, .combine = c) %dopar% {
   list(trial_ref(marker_df[,i], genotype_df),trial_add(zstat_df[,i]))
-  #list(trial2(zstat_df[,i],pvalue_df[,i]))
-  #zstat_df <- as.data.frame(trial$preprocess$Zstat[[i]]) # i
-  #trial2(zstat_df[,i],pvalue_df[,i],marker_df[,i])
-  #zstat_df <- zstat_df[1:5,1:5]
-  #gbj_test(gwas.zstat[,i], gwas.marker[,i], gwas.pvalue[,i], geno[,i], tab.pc)
-  #list(trial_add(zstat_df[,i]))
 }
-
-trial_com <- list(trial_add,trial_ref)
-results <- foreach(f = trial_com, .combine = "c") %dopar% {
-  f
-}
-
+toc()
 results
 
 # Stop the parallel cluster
