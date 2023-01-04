9531da1010cf26c49fce40c6db69326b92ee475b
nirwan1265 <tandukar.nirwan@gmail.com>
2023-01-04 11:49:00 -0500

    update

--- scripts/pvalue_combine.R
+++
@@ -1,8 +0,0 @@
-pvalue.combine <- function(path, filename, chr, pca, organism){
-  preprocess_data <- preprocess(path, filename, chr,  organism)
-  geno <- geno <- geno_ld(chr)
-  pca <- pca_ld()
-  
-  
-  
-}
