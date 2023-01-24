04eb16d3c553719d308ca204f9e906e206379ab5
nirwan1265 <tandukar.nirwan@gmail.com>
2023-01-23 22:29:46 -0500

    update

--- src/maize_overlift.R
+++ src/maize_overlift.R
@@ -74,8 +74,9 @@ pheno_v5$newSNP <- paste0("S1_",pheno_v5$start)
 geno1 <- geno[["geno1"]]
 geno1 <- geno1[pheno_v5$SNP]
 names(geno1) <- pheno_v5$newSNP
-
+geno1[1:4,1:4]
 write.table(geno1,"geno1.txt", quote = F, row.names = F)
+
 
 ###################################################################################################
 x <- as.numeric(gsub("S2_","",names(geno[["geno2"]])))
@@ -103,7 +104,7 @@ geno2 <- geno[["geno2"]]
 geno2 <- geno2[pheno_v5$SNP]
 names(geno2) <- pheno_v5$newSNP
 geno2[1:4,1:4]
-
+write.table(geno2,"geno2.txt", quote = F, row.names = F)
 
 ############
 x <- as.numeric(gsub("S3_","",names(geno[["geno3"]])))
@@ -131,7 +132,7 @@ geno3 <- geno[["geno3"]]
 geno3 <- geno3[pheno_v5$SNP]
 names(geno3) <- pheno_v5$newSNP
 geno3[1:4,1:4]
-
+write.table(geno3,"geno3.txt", quote = F, row.names = F)
 ######3
 x <- as.numeric(gsub("S4_","",names(geno[["geno4"]])))
 x <- data.frame(names(geno[["geno4"]]),as.numeric(gsub("S4_","",names(geno[["geno4"]]))))
@@ -157,7 +158,7 @@ pheno_v5$newSNP <- paste0("S4_",pheno_v5$start)
 geno4 <- geno[["geno4"]]
 geno4 <- geno4[pheno_v5$SNP]
 names(geno4) <- pheno_v5$newSNP
-
+write.table(geno4,"geno4.txt", quote = F, row.names = F)
 
 #####
 x <- as.numeric(gsub("S5_","",names(geno[["geno5"]])))
@@ -184,7 +185,7 @@ pheno_v5$newSNP <- paste0("S5_",pheno_v5$start)
 geno5 <- geno[["geno5"]]
 geno5 <- geno5[pheno_v5$SNP]
 names(geno5) <- pheno_v5$newSNP
-
+write.table(geno5,"geno5.txt", quote = F, row.names = F)
 
 #############
 x <- as.numeric(gsub("S6_","",names(geno[["geno6"]])))
@@ -211,7 +212,7 @@ pheno_v5$newSNP <- paste0("S6_",pheno_v5$start)
 geno6 <- geno[["geno6"]]
 geno6 <- geno6[pheno_v5$SNP]
 names(geno6) <- pheno_v5$newSNP
-
+write.table(geno6,"geno6.txt", quote = F, row.names = F)
 #######
 x <- as.numeric(gsub("S8_","",names(geno[["geno8"]])))
 x <- data.frame(names(geno[["geno8"]]),as.numeric(gsub("S8_","",names(geno[["geno8"]]))))
@@ -239,7 +240,7 @@ geno8 <- geno[["geno8"]]
 geno8 <- geno8[pheno_v5$SNP]
 names(geno8) <- pheno_v5$newSNP
 geno8[1:4,1:4]
-
+write.table(geno8,"geno8.txt", quote = F, row.names = F)
 
 ########3
 x <- as.numeric(gsub("S9_","",names(geno[["geno9"]])))
@@ -268,7 +269,7 @@ geno9 <- geno[["geno9"]]
 geno9 <- geno9[pheno_v5$SNP]
 names(geno9) <- pheno_v5$newSNP
 geno9[1:4,1:4]
-
+write.table(geno9,"geno9.txt", quote = F, row.names = F)
 
 
 #######3x <- as.numeric(gsub("S10_","",names(geno[["geno10"]])))
@@ -297,15 +298,43 @@ geno10 <- geno[["geno10"]]
 geno10 <- geno10[pheno_v5$SNP]
 names(geno10) <- pheno_v5$newSNP
 geno10[1:4,1:4]
-
+write.table(geno10,"geno10.txt", quote = F, row.names = F)
 ###########
 
 system("ls")
-geno7 <- read.table("chr7_MAF_MAF0.01_pheno.txt", header = T)
+#geno7 <- read.table("chr7_MAF_MAF0.01_pheno.txt", header = T)
 geno7 <- vroom("chr7_MAF_MAF0.01_pheno.txt")
-geno7[1:4,1:4]
 
-trial_geno6 <- vroom("chr6_MAF_MAF0.01_pheno.txt")
+
+x <- as.numeric(gsub("S7_","",names(geno7)))
+x <- data.frame(names(geno7),as.numeric(gsub("S7_","",names(geno7))))
+names(x) <- c("SNP","ps")
+x$chr <- 1
+
+
+
+pheno_v2 <- makeGRangesFromDataFrame(x,
+                                     keep.extra.columns=TRUE,
+                                     ignore.strand=TRUE,
+                                     seqinfo = seqinfo(AGPv2),  # Sanity check
+                                     seqnames.field="chr",
+                                     start.field="ps",
+                                     end.field="ps")
+
+
+pheno_v4 <- liftOver(pheno_v2,ch) %>% unlist()
+pheno_v5 <-  liftOver(pheno_v4,ch2) %>% unlist()
+pheno_v5 <- data.frame(pheno_v5)
+pheno_v5$newSNP <- paste0("S7_",pheno_v5$start)
+
+#geno7 <- geno[["geno7"]]
+
+geno7 <- geno7[pheno_v5$SNP]
+names(geno7) <- pheno_v5$newSNP
+geno7[1:4,1:4]
+write.table(geno7,"geno7.txt", quote = F, row.names = F)
+
+
 
 
 
